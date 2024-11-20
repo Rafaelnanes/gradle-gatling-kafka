67650cb2f4364381aada03e82cc734c5bb4ecf6e
Rafael Nanes <rafaelbraga_22@hotmail.com>
2024-11-20 15:08:43 +0000

    initial commit

---
+++ src/main/java/com/example/demo/SimpleController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/root")
+public class SimpleController {
+
+  @GetMapping
+  public String any() {
+    return "Any";
+  }
+
+}
